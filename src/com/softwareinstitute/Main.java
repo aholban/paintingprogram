191b582d07a3d6c5f4997a5b16b0fdeea67c9908
aholban <andreeaholban98@gmail.com>
2021-10-12 15:12:07 +0100

    Painting multiple walls using for loop

--- src/com/softwareinstitute/Main.java
+++ src/com/softwareinstitute/Main.java
@@ -7,37 +7,48 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("What is the height of the wall in metres?");
-        float height = Float.parseFloat(input.nextLine());
-        System.out.println("What is the width of the wall in metres?");
-        float width = Float.parseFloat(input.nextLine());
-        System.out.println("Are there any windows? (yes/no)");
-        String windows = input.nextLine();
-        float areaToPaint = height*width;
-        while(windows.equals("yes")){
-            System.out.println("What is the height of the window?");
-            float windowheight = Float.parseFloat(input.nextLine());
-            System.out.println("What is the width of the window?");
-            float windowwidth = Float.parseFloat(input.nextLine());
-            areaToPaint = areaToPaint - (windowwidth * windowheight);
 
-            System.out.println("Are there any other windows? (yes/no)");
-            windows = input.nextLine();
-        }
-
+        System.out.println("How many walls do you want to paint?");
+        int nwalls = Integer.parseInt(input.nextLine());
         System.out.println("How much does a litre of paint cost in pounds?");
         float price = Float.parseFloat(input.nextLine());
-        float paintNeeded = areaToPaint/6.0f;
+        float totalPaintNeeded = 0;
+        float totalCost = 0;
+        for(int i=1; i<=nwalls; i++) {
 
-        System.out.println("Do you want a second layer of paint? (yes/no)");
-        String layers = input.nextLine();
-        if(layers.equals("yes")){
-            paintNeeded = paintNeeded*2;
+            System.out.println("What is the height of the wall " + i + " in metres?");
+            float height = Float.parseFloat(input.nextLine());
+            System.out.println("What is the width of the wall " + i + " in metres?");
+            float width = Float.parseFloat(input.nextLine());
+            System.out.println("Are there any windows? (yes/no)");
+            String windows = input.nextLine();
+            float areaToPaint = height * width;
+            while (windows.equals("yes")) {
+                System.out.println("What is the height of the window?");
+                float windowheight = Float.parseFloat(input.nextLine());
+                System.out.println("What is the width of the window?");
+                float windowwidth = Float.parseFloat(input.nextLine());
+                areaToPaint = areaToPaint - (windowwidth * windowheight);
+
+                System.out.println("Are there any other windows? (yes/no)");
+                windows = input.nextLine();
+            }
+
+            float paintNeeded = areaToPaint / 6.0f;
+
+            System.out.println("Do you want a second layer of paint? (yes/no)");
+            String layers = input.nextLine();
+            if (layers.equals("yes")) {
+                paintNeeded = paintNeeded * 2;
+            }
+
+            float cost = paintNeeded * price;
+
+            totalPaintNeeded = totalPaintNeeded + paintNeeded;
+            totalCost = totalCost + cost;
         }
-
-        float cost = paintNeeded*price;
-        System.out.println("Paint needed (in litres): " + paintNeeded);
-        System.out.println("Total cost (in pounds): " + cost);
+        System.out.println("Paint needed (in litres): " + totalPaintNeeded);
+        System.out.println("Total cost (in pounds): " + totalCost);
     }
 
 }
